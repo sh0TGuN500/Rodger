1292a9fbcb33da339b137857ad9728d6610fb1a5
sh0TGuN500 <grandma7ter500@gmail.com>
2022-10-02 00:50:45 +0200

    pre release

---
+++ djbook_test/storage_backends.py
@@ -0,0 +1,25 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class PublicMediaStorage(S3Boto3Storage):
+    location = 'media'
+    default_acl = 'public-read'
+    file_overwrite = False
+
+# SFTP
+
+
+'''
+DEFAULT_FILE_STORAGE = 'storages.backends.sftpstorage.SFTPStorage'
+
+SFTP_STORAGE_HOST = '127.0.0.1'
+SFTP_STORAGE_ROOT = '/var/www/media/'
+SFTP_STORAGE_PARAMS = {
+    'username': 'root',
+    'password': 'password',
+    'allow_agent': False,
+    'look_for_keys': False,
+}
+# SFTP_KNOWN_HOST_FILE = '~/.ssh/known_hosts'
+SFTP_STORAGE_INTERACTIVE = False
+'''
