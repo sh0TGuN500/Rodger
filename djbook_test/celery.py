1389aedd53b118e0a7feee81d09bd9e048bd0a20
sh0TGuN500 <grandma7ter500@gmail.com>
2022-10-20 17:29:00 +0200

    big update and small fixes

--- djbook_test/celery.py
+++ djbook_test/celery.py
@@ -3,9 +3,9 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djbook_test.settings')
 
-app = Celery('proj')
+app = Celery('djbook_test')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
