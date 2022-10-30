ded89640306076cbcac1ad42378d764fe1aa5244
sh0TGuN500 <grandma7ter500@gmail.com>
2022-10-30 13:23:36 +0100

    implemented Froala editor

--- djbook_test/settings.py
+++ djbook_test/settings.py
@@ -63,7 +63,6 @@ FROALA_EDITOR_OPTIONS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
